8bc9309481600b7c0fc914e10d7cdf807e6686e7
Joanna <jbrumfield623@gmail.com>
2020-02-15 09:07:40 -0800

    Added assignment 1.4

---
+++ week-1/brumfield-1.4.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 15 February 2020
+; Modified By: Joanna Brumfield
+; Description: This program demonstrates the
+; use of JavaScript variables and the 
+; console.log function for debugging using Node.js
+;===========================================
+*/ 
+
+let firstName = 'Joanna';
+let lastName = 'Brumfield';
+let age = 32;
+let street = 'some random street address';
+let city = 'some random city name';
+let zipCode = 'some random zip code';
+
+console.log( 'First Name: ' + firstName + '\n' + 'Last Name: '+ lastName + 
+            '\n' + 'Age: ' + age + '\n' + 'Street: ' + street + '\n' + 'City: ' 
+            + city + '\n' + 'Zip Code: ' + zipCode ); 
