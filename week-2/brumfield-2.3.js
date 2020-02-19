1a8c1259c35c5b5c1d8155dc2f058393c16ed912
Joanna <jbrumfield623@gmail.com>
2020-02-19 12:22:28 -0800

    Added assignment 2.3 file

---
+++ week-2/brumfield-2.3.js
@@ -0,0 +1,20 @@
+const header = require('../brumfield-header.js');
+/*
+============================================
+; Title:  Brumfield-2.3.js
+; Author: Joanna Brumfield
+; Date:   19 February 2020
+; Description: Demonstrates the declaration and
+; use of JavaScript functions
+;===========================================
+*/
+
+myName.joanna = 'Joanna Brumfield';
+
+function myName() {
+  return myName.joanna;
+};
+
+console.log('Hello ' + myName() + '!');
+
+
