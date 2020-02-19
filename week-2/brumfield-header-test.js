48f8e80adf26ae9a0f7e9bc9c21336ef9812e62b
Joanna <jbrumfield623@gmail.com>
2020-02-19 11:40:07 -0800

    Added and modified headers files for assignment 2.2

---
+++ week-2/brumfield-header-test.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  header.js
+; Author: Joanna Brumfield
+; Date:   19 February 2020
+; Description: Displays a formatted header
+;===========================================
+*/
+
+//Locate and require header file
+const header = require('../brumfield-header.js');
+
+//Input parameters into header function and print to console
+console.log(header.display('Joanna', 'Brumfield', 'Assignment-2.1'));
