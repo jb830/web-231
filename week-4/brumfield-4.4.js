f096bee6bd4cd88a36d8d574df4e4d4d8d057703
Joanna <jbrumfield623@gmail.com>
2020-03-11 18:25:50 -0700

    Added assignment 4.4

--- week-4/brumfield-4.4.js
+++ week-4/brumfield-4.4.js
@@ -3,36 +3,40 @@ const header = require('../brumfield-header.js');
 ============================================
 ; Title:  Brumfield-4.3.js
 ; Author: Professor Krasso
-; Date:   25 February 2020
+; Date:   11 March 2020
 ; Modified By: Joanna Brumfield
 ; Description:
 ;===========================================
 */
-console.log(header.display('Joanna', 'Brumfield', 'Assignment 3.4')+ '\n');
+console.log(header.display('Joanna', 'Brumfield', 'Assignment 4.4')+ '\n');
 
-/*
-  // Below is what I am expecting to see when I run your program - remove this before submitting your work
-  // 50% of you will not read this and lose 25% of the awardable points.
+let states = ['Alabama', 'Nebraska', 'Iowa', 'California', 'Nevada'];
+// * Params: Two Strings
+// * Response: True/false
+// * Description: Checks to see if two string values match.
+function getState(arg, state) {
+  state = 'Iowa'
+  if (arg === state) {
+    return true;
+  }
+}
 
-  FirstName LastName
-  Assignment 4.4
-  Today's Date
+// * Params: One array type parameter
+// * Response: Values of array
+// * Description: Return the values of
+// the array that have been iterated over
+function printStates(arr) {
+  for (let i = 0; i < arr.length; i++) {
+      return arr;
+  }
+}
 
-  -- ORIGINAL ARRAY --
-  Alabama
-  Nebraska
-  Iowa
-  California
-  Nevada
+// * Creates a new array with filtered values
+// based off of arguments in the getState() predicate function
+// which checks to see if string argument matches a value in the array.
+const statesSelection = states.filter(getState);
 
-  -- SORTED ARRAY --
-  Alabama
-  California
-  Iowa
-  Nebraska
-  Nevada
+console.log('\n-- ORIGINAL ARRAY --\n' + printStates(states));
+console.log('\n-- SORTED ARRAY --\n' + states.sort());
+console.log('\n-- SELECTED VALUE --\n' + statesSelection);
 
-  -- SELECTED VALUE --
-  Iowa
-
-*/
