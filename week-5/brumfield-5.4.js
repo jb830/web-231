90f5a4de589ce74e765bfb486fd95e39142faaf7
Joanna <jbrumfield623@gmail.com>
2020-03-18 10:35:20 -0700

    Added assignment 5.4

--- week-5/brumfield-5.4.js
+++ week-5/brumfield-5.4.js
@@ -1,63 +1,69 @@
+const header = require('../brumfield-header.js');
 /*
 ============================================
-; Title:  filtering-reducing-complex-data-structures.js
+; Title:  Brumfield-5.4.js
 ; Author: Professor Krasso
-; Date:   25 June 2017
+; Date:   18 March 2020
+; Modified By: Joanna Brumfield
 ; Description: Demonstrates how to filter list of collection objects
 ;===========================================
 */
-/*
-Effectively, the above code takes a collection of poets and transforms them into an array of String values. The map() method is primarily used for transforming larger data sets into dynamic, complex data types. I know this probably sounds confusing but think of the map() method as a way of mapping fields in one object array to another. Why is this useful? Imagine you were querying an employee table in a database and only wanted to display the employees first name, last name, and address, but the query being used returns all database fields. You could use the map() method to filter out the unwanted fields.
+console.log(header.display('Joanna', 'Brumfield', 'Assignment 5.4')+ '\n');
 
-Instructions
-
-Using the array created in exercise 5.3, complete the following:
-
-1. Using JavaScripts built-in map() function, create a filtered
-    array of composers by rating (review the starter code for output expectations).
-2. Using JavaScripts built-in map() function, create a filtered
-    array of composers by genre (review the starter code for output expectations).
-3. Using JavaScripts built-in forEach() function, iterate over each array and
-  output the results.
-4. Run and test the program and make sure your output matches what I have
-  in the starter code.
-5. Stage, commit and push your work to GitHub.
-*/
-
-
-// object collection
-const famousPoets = [
+// Collection of composer objects
+const famousComposers = [
   {
-    firstName: "William",
-    lastName: "Wordsworth"
+    firstName: 'Ludwig van',
+    lastName: 'Beethoven',
+    genre: 'Classical',
+    rating: 8
   },
   {
-    firstName: "William",
-    lastName: "Shakespeare"
+    firstName: 'Wolfgang',
+    lastName: 'Mozart',
+    genre: 'Classical',
+    rating: 10
   },
   {
-    firstName: "Percy",
-    lastName: "Shelley"
+    firstName: 'Johann',
+    lastName: 'Bach',
+    genre: 'Classical',
+    rating: 9
   },
   {
-    firstName: "Robert",
-    lastName: "Frost"
+    firstName: 'Joseph',
+    lastName: 'Haydn',
+    genre: 'Classical',
+    rating: 6
   },
   {
-    firstName: "Maya",
-    lastName: "Angelou"
+    firstName: 'Franz',
+    lastName: 'Schubert',
+    genre: 'Classical',
+    rating: 5
   }
-]
+];
 
-// return a new list of objects with only the lastName field
-let lastNames = famousPoets.map((poet) => {
-  return poet.lastName
-})
+// Creates a filtered array of composers by rating using the built-in map() function
+let composerRatings = famousComposers.map(function (composer) {
+  return 'Rating: ' + composer.rating + '\nComposer: ' + composer.lastName + '\n';
+});
 
-// output
-console.log('-- DISPLAYING MY FAVORITE COMPOSERS LAST NAMES --')
-index = 1
-lastNames.forEach((lastName) => {
-  console.log(index + '. ' + lastName)
-  index++
-})
+// Creates a filtered array of composers by genre using the built-in map() function
+let composerGenres = famousComposers.map(function (composer) {
+  return 'Genre: ' + composer.genre + '\nComposer: ' + composer.lastName + '\n';
+});
+
+// Iterates over the composerRatings array build with the map() function
+// and logs the results
+console.log('-- COMPOSER BY RATING --')
+composerRatings.forEach(function(genres) {
+  console.log(genres);
+});
+
+// Iterates over the composerGenres array build with the map() function
+// and logs the results
+console.log('\n-- COMPOSER BY GENRE --')
+composerGenres.forEach(function(genres) {
+  console.log(genres);
+});
