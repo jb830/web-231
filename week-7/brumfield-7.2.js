615352e607dfcc42152358d6c28560a63992074a
Joanna <jbrumfield623@gmail.com>
2020-03-29 13:16:13 -0700

    Added week 7 assignments

---
+++ week-7/brumfield-7.2.js
@@ -0,0 +1,42 @@
+const header = require('../brumfield-header.js');
+console.log(header.display('Joanna', 'Brumfield', 'Assignment 7.2')+ '\n');
+/*
+============================================
+; Title:  brumfield-7.2.js
+; Author: Professor Krasso
+; Date:   28 March 2020
+; Modified By: Joanna Brumfield
+; Description: Demonstrates creating a
+; constructor object and logging its values.
+;===========================================
+*/
+// 1. Create an employee constructor object with four (4) parameters/fields
+//    id
+//    firstName
+//    lastName
+//    title
+// 2. Populate an array with five (5) employee objects
+// 3. Loop over the array and output the results
+
+function Employee( id, firstName, lastName, title ) {
+  this.id = id;
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.title = title;
+}
+
+let employees = [
+  new Employee( 43245, 'Thomas', 'Edison', 'Software Engineer' ),
+  new Employee( 76576, 'Benjamin', 'Franklin', 'Programmer' ),
+  new Employee( 98798, 'Nikola', 'Tesla', 'Project Manager' ),
+  new Employee( 98793, 'Charles', 'Babbage', 'Product Manager'),
+  new Employee( 65765, 'Alexander', 'Bell Business', 'Analyst')
+];
+
+let counter = 0;
+for ( i = 0; i < employees.length; i++ ) {
+  counter++;
+  console.log(counter + ".", employees[i].firstName, employees[i].lastName, employees[i].title);
+};
+
+
---
+++ week-7/brumfield-7.3.js
@@ -0,0 +1,52 @@
+/*
+============================================
+; Title:  brumfield-6.4.js
+; Author: Professor Krasso
+; Date:   29 March 2020
+; Modified By: Joanna Brumfield
+; Description: Demonstrates manipulating the
+; DOM via various document methods and properties
+;===========================================
+*/
+
+//writes an ordered list of
+document.write(
+  '<h1>SHOPPING LIST v.1</h1></h1><ol><li>Oranges</li><li>Apples</li><li>Pears</li><li>Limes</li><li>Lemons</li></ol>'
+  );
+
+window.onload = writeList;
+function writeList() {
+  document.getElementById("list").innerHTML =
+    "<h1>SHOPPING LIST v.2</h1>" +
+    "<table>" +
+      "<thead>" +
+        "<tr>" +
+          "<th>ID</th>" +
+          "<th>Item</th>" +
+        "</tr>" +
+      "</thead>" +
+      "<tbody>" +
+        "<tr>" +
+          "<td>1</td>" +
+          "<td>Oranges</td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>2</td>" +
+          "<td>Apples</td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>3</td>" +
+          "<td>Pears</td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>4</td>" +
+          "<td>Limes</td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>5</td>" +
+          "<td>Lemons</td>" +
+        "</tr>" +
+      "</tbody>" +
+    "</table>"
+  ;
+};
